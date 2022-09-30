ad4fba06ef2987016df0ea4fddf3ea68becdb769
christiannjhay-stu <74962185+christiannjhay-stu@users.noreply.github.com>
2022-09-30 15:37:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
+const express = require("express")
+const app = express()
 
+console.log("Working")
+
+
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) =>{
+    console.log("here")
+    res.render("index")
+})
+
+app.listen(3000)
